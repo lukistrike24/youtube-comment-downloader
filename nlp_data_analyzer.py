c06d3df138e6b98227b76f0273dc6af6659b66d7
Lukas Höfler <lu.hoefler@gmail.com>
2020-05-02 23:15:20 +0200

    simple dataanalysis

--- nlp_data_analyzer.py
+++ nlp_data_analyzer.py
@@ -5,6 +5,7 @@ Created on Sat May  2 22:20:34 2020
 @author: luhoe
 """
 import json
+from collections import Counter
 
 video_url = 'https://www.youtube.com/watch?v=IvGBZaprpWE'
 data_folder = "C:\\Users\\luhoe\\Documents\\Git_Projects\\Github\\youtube-comment-downloader\\Data"
@@ -13,3 +14,15 @@ ID =  video_url.split('=')[-1]
 
 f = open(data_folder + '\\' + ID + '.json')
 data = json.load(f)
+
+comments = data['comments']
+
+def getAuthors(comments):
+    Authors = []
+    for comment in comments:
+        Authors.append(comments[comment]['author'])    
+    return Authors
+    
+Authors = getAuthors(comments)
+c = Counter(Authors)
+most_common_Authors =  c.most_common(50)
