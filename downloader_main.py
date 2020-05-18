5c8c6c4081cb17c8bae8a381bf4f1f2de8230aac
Lukas Höfler <lu.hoefler@gmail.com>
2020-05-18 20:49:00 +0200

    added download lists

--- downloader_main.py
+++ downloader_main.py
@@ -11,36 +11,44 @@ from comments_downloader import downloader_main
 from description_downloader import scrape_video_data
 from datetime import datetime
 
-video_url = 'https://www.youtube.com/watch?v=nXWzbsnb638'
-output_folder = "C:\\Users\\luhoe\\Documents\\Git_Projects\\Github\\youtube-comment-downloader\\Data"
+urlList = 'C:\\Users\\luhoe\\Documents\\Git_Projects\\Github\\youtube-comment-downloader\\download_urls\\galileo_urls2.txt'
 
+with open(urlList) as f:
+    content = f.readlines()
+content = [x.strip() for x in content]
 
-argv = {}
-argv['youtubeid'] = video_url.split('=')[-1]
-argv['output'] = output_folder
-argv['limit'] = 1000
-
-
-# Create if directory does not exist
-if not os.path.exists(argv['output']):
-    os.makedirs(argv['output'])
-
-description_dict = scrape_video_data(video_url)
-
-comments_dict = downloader_main(argv)
-
-VideoData = {}
-VideoData['description'] = description_dict
-VideoData['comments'] = comments_dict
-VideoData['size'] = len(comments_dict)
-VideoData['download_date'] = datetime.today().strftime('%Y-%m-%d')
-
-
-
-
-#save to json text file
-print('saving collected data to json file')
-with open(os.path.join(argv['output'],argv['youtubeid'] + '.json'), 'w') as fw:
-	fw.write(json.dumps(VideoData))
+for video_url in content:
+    
+    #video_url = 'https://www.youtube.com/watch?v=nXWzbsnb638'
+    output_folder = "C:\\Users\\luhoe\\Documents\\Git_Projects\\Github\\youtube-comment-downloader\\Data"
+    
+    
+    argv = {}
+    argv['youtubeid'] = video_url.split('=')[-1]
+    argv['output'] = output_folder
+    argv['limit'] = 1000
+    
+    
+    # Create if directory does not exist
+    if not os.path.exists(argv['output']):
+        os.makedirs(argv['output'])
+    
+    description_dict = scrape_video_data(video_url)
+    
+    comments_dict = downloader_main(argv)
+    
+    VideoData = {}
+    VideoData['description'] = description_dict
+    VideoData['comments'] = comments_dict
+    VideoData['size'] = len(comments_dict)
+    VideoData['download_date'] = datetime.today().strftime('%Y-%m-%d')
+    
+    
+    
+    
+    #save to json text file
+    print('saving collected data to json file')
+    with open(os.path.join(argv['output'],argv['youtubeid'] + '.json'), 'w') as fw:
+    	fw.write(json.dumps(VideoData))
 
 print('everything  finished!')
